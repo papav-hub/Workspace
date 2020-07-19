a2ecd3734b1c1f1baf796f6e389658f62f846752
Hyemin <Hyemin@DESKTOP-4UKNU1A>
2020-07-19 12:15:56 +0900

    substring

---
+++ Workspace/src/variable/Cut.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Cut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello World";
+		System.out.println(s.substring(2, 6));
+
+	}
+
+}
