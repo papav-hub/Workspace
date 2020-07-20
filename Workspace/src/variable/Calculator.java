9a69c11a7626316e7d425ef0faaee96aa6a08ba0
Hyemin <Hyemin@DESKTOP-4UKNU1A>
2020-07-20 10:59:23 +0900

    계산기

--- Workspace/src/variable/Calculator.java
+++ Workspace/src/variable/Calculator.java
@@ -4,7 +4,13 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1 = 11;
+		int num2 = 22;
 		
+		System.out.println("더하기 : " + (num1+num2));
+		System.out.println("빼기 : " + (num1-num2));
+		System.out.println("곱하기 : " + (num1*num2));
+		System.out.println("나누기 : " + ((double)num1/(double)num2));
 
 	}
 
