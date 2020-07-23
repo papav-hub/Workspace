b8ed3bd3e4ef1a4acbb4191b860d350f74bcb732
Hyemin <Hyemin@DESKTOP-4UKNU1A>
2020-07-23 10:23:31 +0900

    예쁘게 만들기

--- Workspace/src/shape/Circle.java
+++ Workspace/src/shape/Circle.java
@@ -5,21 +5,21 @@ public class Circle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<=3;i++) {
-			for(int j=(3-i)*2;j>=1;j--) {
+		for(int i = 1; i <= 3; i++) {
+			for(int j = (3-i)*2; j >= 1; j--) {
 				System.out.print(" ");
 			}
-			for(int j=1;j<=i*4-1;j++) {
+			for(int j = 1; j <= i*4-1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		for(int i=3;i>=1;i--) {
-			for(int j=(3-i)*2;j>=1;j--) {
+		for(int i = 3; i >= 1; i--) {
+			for(int j = (3-i)*2; j >= 1; j--) {
 				System.out.print(" ");
 			}
-			for(int j=1;j<=i*4-1;j++) {
+			for(int j = 1; j <= i*4-1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
