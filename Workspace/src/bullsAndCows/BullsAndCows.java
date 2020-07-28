190a607e619bf10f26c05b1e1f492709cdc55c2e
혜민 최 <pavi03@kpu.ac.kr>
2020-07-28 15:13:40 +0900

    3자리 숫자야구

---
+++ Workspace/src/bullsAndCows/BullsAndCows.java
@@ -0,0 +1,75 @@
+package bullsAndCows;
+
+import java.util.Scanner;
+
+public class BullsAndCows {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int result[] = new int[3]; // 정답 정하기	
+		result[0] = (int)(Math.random() * 9) + 1; // 첫번째 수는 0 안됨
+		for(int i = 1 ; i < result.length ; i++) {
+			result[i] = (int)(Math.random() * 10);
+			for(int j = 0 ; j < i ; j++) { // 중복제거
+				if(result[i] == result[j]) {
+					i--;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < result.length ; i++) { // 정답 출력 // 확인용
+			System.out.print(result[i] + " ");
+		}
+		
+		while(true) {
+			
+		int input[] = new int[3]; // 사용자한테서 입력받기
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 : ");
+		for(int i = 0 ; i < input.length ; i++) {
+			input[i] = sc.nextInt();	
+		}
+		
+		int check = 1;
+		for(int i = 0 ; i < result.length ; i++) {// 정답 입력하면 break;
+			if(result[i]!=input[i]) {
+				check = 0;
+			}
+		}		
+		if(check==1) {
+			System.out.println("정답입니다!");
+			break;
+		}
+		
+		
+		
+		int strike = 0;
+		int ball = 0;
+		
+		for(int i = 0 ; i < result.length ; i++) {
+			for(int j = 0 ; j < input.length ; j++) {
+				if(result[i] == input[j]) {
+					if(i==j) {
+						strike++;
+					}else {
+						ball++;
+					}
+				}
+			}
+		}
+		
+		if((strike==0)&&(ball==0)) {
+			System.out.println("Out");
+		}else {
+			System.out.println("strike : " + strike + "\nball : " + ball);
+		}
+		
+		
+
+		}
+
+	}
+
+}
