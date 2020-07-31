b50cecf329f2c70bbb950be7433dd2bdff2e780d
혜민 최 <pavi03@kpu.ac.kr>
2020-07-31 17:14:46 +0900

    static 안쓰고 함수 만들기

--- Workspace/src/bullsAndCows/BullsAndCows.java
+++ Workspace/src/bullsAndCows/BullsAndCows.java
@@ -3,11 +3,10 @@ package bullsAndCows;
 import java.util.Scanner;
 
 public class BullsAndCows {
+
 	
-	static int result[] = new int[4];
-	static int input[] = new int[4];
-	
-	static void setResult() { // 정답 정하기	
+	static int[] setResult() { // 정답 정하기	
+		int result[] = new int[4];
 		result[0] = (int)(Math.random() * 9) + 1; // 첫번째 수는 0 안됨
 		for(int i = 1 ; i < result.length ; i++) {
 			result[i] = (int)(Math.random() * 10);
@@ -22,15 +21,25 @@ public class BullsAndCows {
 			System.out.print(result[i] + " ");
 		}
 		System.out.println();
+		
+		return result;
 	}
 	
-	static void userResult() {
-		while(true) { // 사용자한테서 입력받기
-			Scanner sc = new Scanner(System.in);
-			System.out.print("숫자 : ");
-			for(int i = 0 ; i < input.length ; i++) {
-				input[i] = sc.nextInt();	
-			}
+	static int[] userInput() {
+		int input[] = new int[4];
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 : ");
+		for(int i = 0 ; i < input.length ; i++) {
+			input[i] = sc.nextInt();	
+		}
+		return input;
+	}
+	
+	static void resultResult(int[] result) {
+		while(true) {
+			
+			int input[]; // 사용자한테서 입력받기
+			input = userInput();
 			
 			int check = 1;
 			for(int i = 0 ; i < result.length ; i++) {// 정답 입력하면 break;
@@ -43,40 +52,38 @@ public class BullsAndCows {
 				break;
 			}
 			
-			printResult();
+			int strike = 0;
+			int ball = 0;
 			
-		}
-	}
-		
-	static void printResult() {
-		int strike = 0;
-		int ball = 0;
-		
-		for(int i = 0 ; i < result.length ; i++) {
-			for(int j = 0 ; j < input.length ; j++) {
-				if(result[i] == input[j]) {
-					if(i==j) {
-						strike++;
-					}else {
-						ball++;
+			for(int i = 0 ; i < result.length ; i++) {
+				for(int j = 0 ; j < input.length ; j++) {
+					if(result[i] == input[j]) {
+						if(i==j) {
+							strike++;
+						}else {
+							ball++;
+						}
 					}
 				}
 			}
+			strikeBall(strike, ball);
 		}
-		
+	}
+	
+	static void strikeBall(int strike, int ball) {
 		if((strike == 0) && (ball == 0)) {
 			System.out.println("Out");
 		}else {
 			System.out.println("strike : " + strike + "\nball : " + ball);
 		}
-
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		setResult();
-		userResult();
+		int result[];
+		result = setResult();
+		resultResult(result);
 		
 	}
 
