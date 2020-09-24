a78a0d7f3828952b1ab90eb93773ce3cf5de36ce
papav-hub <pavi03@kpu.ac.kr>
2020-09-24 17:13:21 +0900

    class 하나 당 작업파일 하나 만들기 연습하기

--- Workspace/src/javaH/Ex4_5.java
+++ Workspace/src/javaH/Ex4_5.java
@@ -22,7 +22,7 @@ public class Ex4_5 { // Ex4_5 = Book
 		// TODO Auto-generated method stub
 		Ex4_5 littlePrince = new Ex4_5("어린왕자", "생텍쥐페리");
 		Ex4_5 loveStory = new Ex4_5("춘향전");
-		Ex4_5 emptyBook = new Ex4_5(); // this("", "");수행하고, 생성자 호출됨 출력
+		Ex4_5 emptyBook = new Ex4_5(); // this("비어있음", "비어있음"); 수행하고, 생성자 호출됨 출력
 		littlePrince.show();
 		loveStory.show();
 		emptyBook.show();
---
+++ Workspace/src/javaH/Ex4_8.java
@@ -0,0 +1,29 @@
+package javaH;
+
+public class Ex4_8 { //Ex4_8 = ArrayParameterEx
+	
+	static void replaceSpace(char a[]) {
+		for (int i=0 ; i<a.length ; i++) {
+			if(a[i] == ' ') {
+				a[i] = ',';
+			}
+		}
+	}
+	
+	static void printCharArray(char a[]) {
+		for (int i=0 ; i<a.length ; i++) {
+			System.out.print(a[i]);
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c[] = {'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.'};
+		
+		printCharArray(c);
+		
+		replaceSpace(c);
+		printCharArray(c);
+	}
+}
