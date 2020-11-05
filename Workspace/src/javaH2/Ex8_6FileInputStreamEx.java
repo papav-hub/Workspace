df27adc67e8c680ee33ff4c3484b2523db587331
papav-hub <pavi03@kpu.ac.kr>
2020-11-05 16:51:12 +0900

    자바

---
+++ Workspace/src/javaH2/Ex8_6FileInputStreamEx.java
@@ -0,0 +1,29 @@
+package javaH2;
+import java.io.*;
+public class Ex8_6FileInputStreamEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte b[] = new byte[6];
+		try {
+			FileInputStream fin = new FileInputStream("c:\\Temp\\test.out");
+			
+			int n = 0, c;
+			while((c = fin.read())!= -1) {
+				b[n] = (byte)c;
+				n++;
+			}
+			System.out.println("c:\\Temp\\test.out에서 읽은 배열을 출력합니다.");
+			for(int i=0 ; i<b.length ; i++) {
+				System.out.print(b[i] + " ");
+			}
+			System.out.println();
+			fin.close();
+		}catch(IOException e) {
+			System.out.println("c:\\Temp\\test.out에서 읽지 못했습니다. 경로명을 확인해 주세요");
+			return;
+		}
+	}
+
+}
