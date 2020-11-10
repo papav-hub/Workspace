c1e10c5372f561fd80a47dc218ddf8f4b64c914c
papav-hub <pavi03@kpu.ac.kr>
2020-11-10 15:36:42 +0900

    자바

---
+++ Workspace/src/javaH2/Ex8_7BufferedIOEx.java
@@ -0,0 +1,25 @@
+package javaH2;
+import java.io.*;
+import java.util.Scanner;
+public class Ex8_7BufferedIOEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FileReader fin = null;
+		int c;
+		try {
+			fin = new FileReader("c:\\Temp\\test2.txt");
+			BufferedOutputStream out = new BufferedOutputStream(System.out, 5);
+			while((c=fin.read())!=-1) {
+				out.write(c);
+			}
+			new Scanner(System.in).nextLine();
+			out.flush();
+			fin.close();
+			out.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
